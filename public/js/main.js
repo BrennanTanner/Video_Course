bcc0701ffd0089da2bbead08cddf2c608e18e76e
Brennan Tanner <tan18010@byui.edu>
2022-02-19 02:10:09 -0700

    personalized it

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,36 @@ function menuToggleClickHandler() {
 
 backdrop.addEventListener('click', backdropClickHandler);
 menuToggle.addEventListener('click', menuToggleClickHandler);
+
+function togglePosterView(){
+  if (document.getElementById('overlay1').style.visibility =='visible'){
+    document.getElementById('overlay1').style.visibility='hidden';
+  }
+  else{
+  document.getElementById('overlay1').style.visibility='visible';
+  document.getElementById('overlay2').style.visibility='hidden';
+  document.getElementById('overlay3').style.visibility='hidden';
+}
+};
+
+function toggleShirtView(){
+  if (document.getElementById('overlay2').style.visibility =='visible'){
+    document.getElementById('overlay2').style.visibility='hidden';
+  }
+  else{
+  document.getElementById('overlay1').style.visibility='hidden';
+  document.getElementById('overlay2').style.visibility='visible';
+  document.getElementById('overlay3').style.visibility='hidden';
+  }
+};
+
+function toggleStickerView(){
+  if (document.getElementById('overlay3').style.visibility =='visible'){
+    document.getElementById('overlay3').style.visibility='hidden';
+  }
+  else{
+  document.getElementById('overlay1').style.visibility='hidden';
+  document.getElementById('overlay2').style.visibility='hidden';
+  document.getElementById('overlay3').style.visibility='visible';
+  }
+};
